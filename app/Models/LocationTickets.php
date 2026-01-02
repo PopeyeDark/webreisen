43648c718e115418d188d784d3af5cc1e73d21e8
Joel Xaverl <joxabelajar@gmail.com>
2026-01-02 22:49:23 +0700

    Create Models Categories Locations LocationTickets

--- app/Models/LocationTickets.php
+++ app/Models/LocationTickets.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class LocationTickets extends Model
 {
-    //
+    protected $table = 'location_ticket';
+
+    protected $fillable = [
+        'location_id',
+        'ticket_id',
+        'ticket_category_id',
+    ];
+
+    public function location()
+    {
+        return $this->belongsTo(Locations::class);
+    }
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(TicketCategory::class, 'ticket_category_id');
+    }
 }
