4e4b0d582bddb54d7996db2605f376f95957c396
Joel Xaverl <joxabelajar@gmail.com>
2026-01-02 23:18:46 +0700

    Coding Models TicketCategory Transaction User

--- database/migrations/2026_01_01_162600_create_ticket_categories_table.php
+++ database/migrations/2026_01_01_162600_create_ticket_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique(); //VIP, Reguler
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
