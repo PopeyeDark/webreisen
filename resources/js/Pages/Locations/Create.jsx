362496903426748964c0e11f9b53610df51c968b
Joel Xaverl <joxabelajar@gmail.com>
2026-01-10 23:49:35 +0700

    Edit Locations

--- resources/js/Pages/Locations/Create.jsx
+++ resources/js/Pages/Locations/Create.jsx
@@ -1,12 +1,11 @@
 import { useState, useRef, useEffect } from "react";
-
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import Container from '@/Components/Container';
-import Card from '@/Components/Card';
-import Input from '@/Components/Input';
-import Button from '@/Components/Button';
-import { Head, useForm, usePage } from '@inertiajs/react';
-import { Editor } from '@tinymce/tinymce-react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import Container from "@/Components/Container";
+import Card from "@/Components/Card";
+import Input from "@/Components/Input";
+import Button from "@/Components/Button";
+import { Head, useForm, usePage } from "@inertiajs/react";
+import { Editor } from "@tinymce/tinymce-react";
 import Map from "@/Components/frontend/Map";
 import Swal from "sweetalert2";
 
@@ -15,16 +14,16 @@ export default function Create({auth}) {
 
     // define state with helper inertia
     const {data, setData, post, errors, progress} = useForm({
-        title: '',
-        description: '',
-        officehours: '',
-        category_id: '',
+        title: "",
+        description: "",
+        officehours: "",
+        category_id: "",
         region_id: "",
         ticket_ids: {},
-        phone: '',
-        address: '',
-        latitude: '',
-        longitude: '',
+        phone: "",
+        address: "",
+        latitude: "",
+        longitude: "",
         image: [], // diset array karena multiple images
     });
 
@@ -45,17 +44,17 @@ export default function Create({auth}) {
 
         // Membuat FormData manual supaya file multiple bisa dikirim
         const formData = new FormData();
-        formData.append('title',data.title);
-        formData.append('description',data.description);
-        formData.append('officehours',data.officehours);
-        formData.append('category_id',data.category_id);
-        formData.append('address',data.address);
-        formData.append('latitude',data.latitude);
-        formData.append('longitude',data.longitude);
+        formData.append("title",data.title);
+        formData.append("description",data.description);
+        formData.append("officehours",data.officehours);
+        formData.append("category_id",data.category_id);
+        formData.append("address",data.address);
+        formData.append("latitude",data.latitude);
+        formData.append("longitude",data.longitude);
 
         // Loop file dan tambahkan ke FormData
         for(let i = 0; i < data.image.length; i++) {
-            formData.append('image[]',data.image[i]);
+            formData.append("image[]",data.image[i]);
         }
 
         // Mengambil ticket_ids dari data dan mengubahnya menjadi array
@@ -66,7 +65,7 @@ export default function Create({auth}) {
             formData.append(`ticket_ids[${index}]`, id);
         });
 
-        post(route('locations.store'), {
+        post(route("locations.store"), {
             data:formData,
             forceFormData: true, // supaya inertia tahu ini adalah FormData
         });
@@ -114,40 +113,40 @@ export default function Create({auth}) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className='text-xl font-semibold leading-tight text-gray-800'>Add New Location</h2>}
+            header={<h2 className="text-xl font-semibold leading-tight text-gray-800">Add New Location</h2>}
         >
-            <Head title={'Add Location'}/>
+            <Head title={"Add Location"}/>
             <Container>
-                <Card title={'Add Location'}>
+                <Card title={"Add Location"}>
                     <form onSubmit={handleSubmit}>
-                        <Input label="Title" value={data.title} onChange={(e) => setData('title', e.target.value )} errors={errors.title} />
+                        <Input label="Title" value={data.title} onChange={(e) => setData("title", e.target.value )} errors={errors.title} />
                         {/* TinyMCE Editor */}
                         <label className="block mb-1 font-semibold">Description</label>
                         <Editor
-                            apiKey="babsfkde0ygouwy8ntz8fsci0iqdgz10swomq85qgpzo13rh" // Untuk sementara saya pake apiKey dari mas "ahmad hafidh" dulu, tapi nanti saya ganti dgn punya saya dgn cara daftar di https://www.tiny.cloud/
+                            apiKey="r0u8dhzg622xhn9z31wbjhn8dqe3mvze5zypqygsuwb4lbe" // Untuk sementara saya pake apiKey dari mas "ahmad hafidh" dulu, tapi nanti saya ganti dgn punya saya dgn cara daftar di https://www.tiny.cloud/
                             value={data.description}
                             init={{
                                 height: 300,
                                 menubar: false,
                                 plugins: [
-                                    'advlist autolink lists link image charmap print preview anchor',
-                                    'searchreplace visualblocks code fullscreen',
-                                    'insertdatetime media table paste code help wordcount'
+                                    "advlist autolink lists link image charmap print preview anchor",
+                                    "searchreplace visualblocks code fullscreen",
+                                    "insertdatetime media table paste code help wordcount"
                                 ],
                                 toolbar:
-                                    'undo redo | formatselect | bold italic backcolor | ' +
-                                    'alignleft aligncenter alignright alignjustify | ' +
-                                    'bullist numlist outdent indent | removeformat | help',
+                                    "undo redo | formatselect | bold italic backcolor | " +
+                                    "alignleft aligncenter alignright alignjustify | " +
+                                    "bullist numlist outdent indent | removeformat | help",
                             }}
-                            onEditorChange={(content) => setData('description', content)}
+                            onEditorChange={(content) => setData("description", content)}
                         />
                         {errors.description && <div className="mt-1 text-sm text-red-500">{errors.description}</div>}
 
-                        <Input label="Office Hours" value={data.officehours} onChange={(e) => setData('officehours', e.target.value )} errors={errors.officehours} />
-                        <Input label="Phone" value={data.phone} onChange={(e) => setData('phone', e.target.value )} errors={errors.phone} />
-                        <Input label="Address" value={data.address} onChange={(e) => setData('address', e.target.value )} errors={errors.address} />
-                        <Input label="Latitude" value={data.latitude} onChange={(e) => setData('latitude', e.target.value )} errors={errors.latitude} />
-                        <Input label="Longitude" value={data.longitude} onChange={(e) => setData('longitude', e.target.value )} errors={errors.longitude} />
+                        <Input label="Office Hours" value={data.officehours} onChange={(e) => setData("officehours", e.target.value )} errors={errors.officehours} />
+                        <Input label="Phone" value={data.phone} onChange={(e) => setData("phone", e.target.value )} errors={errors.phone} />
+                        <Input label="Address" value={data.address} onChange={(e) => setData("address", e.target.value )} errors={errors.address} />
+                        <Input label="Latitude" value={data.latitude} onChange={(e) => setData("latitude", e.target.value )} errors={errors.latitude} />
+                        <Input label="Longitude" value={data.longitude} onChange={(e) => setData("longitude", e.target.value )} errors={errors.longitude} />
 
                         <div className="my-4">
                             <Input
@@ -180,7 +179,7 @@ export default function Create({auth}) {
 
                         {/* Category Select */}
                         <label className="block">Category</label>
-                        <select value={data.category_id} onChange={e => setData('category_id', e.target.value)} className="w-full p-2 mb-4 border rounded-sm">
+                        <select value={data.category_id} onChange={e => setData("category_id", e.target.value)} className="w-full p-2 mb-4 border rounded-sm">
                             <option value="">Select Category</option>
                             {categories.map((category) => (
                                 <option key={category.id} value={category.id}>{category.name}</option>
@@ -190,7 +189,7 @@ export default function Create({auth}) {
 
                         {/* Region Select */}
                         <label className="block">Region</label>
-                        <select value={data.region_id} onChange={e =>setData('region_id', e.target.value)} className="w-full p-2 mb-4 border rounded-sm">
+                        <select value={data.region_id} onChange={e =>setData("region_id", e.target.value)} className="w-full p-2 mb-4 border rounded-sm">
                             <option value="">Select Region</option>
                             {regions.map((region) => (
                                 <option key={region.id} value={region.id}>{region.name}</option>
@@ -230,7 +229,7 @@ export default function Create({auth}) {
                         <input
                             type="file"
                             multiple
-                            onChange={(e) => setData('image', e.target.files)}
+                            onChange={(e) => setData("image", e.target.files)}
                             className="mt-2"
                         />
                         {errors.image && <div className="mt-1 text-sm text-red-500">{errors.image}</div>}
@@ -243,9 +242,9 @@ export default function Create({auth}) {
                         )}
 
                         {/* Buttons */}
-                        <div className='flex items-center gap-2'>
-                            <Button type={'submit'} label="Submit"/>
-                            <Button type={'cancel'}  url={route('locations.index')} label="cancel"/>
+                        <div className="flex items-center gap-2">
+                            <Button type={"submit"} label="Submit"/>
+                            <Button type={"cancel"}  url={route("locations.index")} label="cancel"/>
                         </div>
                     </form>
                 </Card>
